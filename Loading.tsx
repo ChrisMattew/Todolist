d85966c7aff529be8a707def0cc1bf63940bc3dd
ChrisMattew <cristian.matteu@gmail.com>
2022-04-14 09:15:05 +0200

    1 file modified

--- Loading.tsx
+++ Loading.tsx
@@ -3,13 +3,10 @@ import React, { useState, useEffect } from 'react';
 const Loading = () => {
   const [loading, setLoading] = useState(false);
   useEffect(() => {
-    setTimeout(
-      () => {
-        setLoading(true);
-      },
-      0,
-      setLoading(false)
-    );
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
   }, []);
 
   return (
